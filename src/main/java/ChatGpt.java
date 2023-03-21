d977ba592edbe9114a9c010aeb66614e66be4ea6
Sudip chatterjee <sudip.chatterjee@unb.ca>
2023-03-21 17:46:01 -0300

    chatgpt REST API

--- src/main/java/ChatGpt.java
+++ src/main/java/ChatGpt.java
@@ -14,13 +14,12 @@ public class ChatGpt
     public static void chatGPT(String text) throws Exception
     {
         String url = "https://api.openai.com/v1/completions";
-        //String chatUrl = "https://api.openai.com/v1/chat/completions";
 
         HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
 
         con.setRequestMethod("POST");
         con.setRequestProperty("Content-Type", "application/json");
-        con.setRequestProperty("Authorization", "Bearer <API Keys>");
+        con.setRequestProperty("Authorization", "Bearer <API Key>");
 
         Map<String, String > hm = new HashMap<>();
         hm.put("role", "user");
@@ -28,9 +27,7 @@ public class ChatGpt
 
         JSONObject data = new JSONObject();
         data.put("model", "text-davinci-003");
-        //data.put("model", "gpt-3.5-turbo");
         data.put("prompt", text);
-        //data.put("messages", hm);
         data.put("max_tokens", 4000);
         data.put("temperature", 0.2);
 
@@ -39,17 +36,18 @@ public class ChatGpt
 
         String output = new BufferedReader(new InputStreamReader(con.getInputStream())).lines()
                 .collect(Collectors.joining());
+        String op = new JSONObject(output).getJSONArray("choices").getJSONObject(0).getString("text");
         System.out.println(new JSONObject(output).getJSONArray("choices").getJSONObject(0).getString("text"));
     }
     public static void main(String[] args) throws Exception {
         while(true) {
             Scanner sc = new Scanner(System.in);
-            String inputString = sc.nextLine();
-            if("Break".equalsIgnoreCase(inputString) || "Exit".equalsIgnoreCase(inputString)){
+            String inputText = sc.nextLine();
+            if("Break".equalsIgnoreCase(inputText) || "Exit".equalsIgnoreCase(inputText)){
                 System.exit(0);
             }
             else{
-                chatGPT(inputString);
+                chatGPT(inputText);
             }
         }
     }
